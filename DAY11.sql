e67bbc2f8cdcb2dfd2a2f760d37f94e8a985e17b
withuw <147489559+withuw@users.noreply.github.com>
2023-10-22 00:45:25 +0300

    Update DAY11.sql

--- DAY11.sql
+++ DAY11.sql
@@ -5,3 +5,23 @@ WHERE Country.Code = City.CountryCode
 GROUP BY Country.Continent;
 
 --EX2
+SELECT
+round(cast(SUM(CASE WHEN texts.signup_action='Confirmed' then 1 else 0 END) as decimal)
+/COUNT(distinct emails.email_id), 2) AS activation_rate
+FROM emails
+LEFT JOIN texts
+  ON emails.email_id = texts.email_id;
+--EX3
+SELECT 
+b.age_bucket,
+round(SUM(CASE WHEN a.activity_type= 'send' THEN time_spent ELSE 0 END)*100.0
+/SUM(a.time_spent),2)
+AS send_perc,
+round(SUM(CASE WHEN a.activity_type= 'open' THEN time_spent ELSE 0 END)*100.0
+/SUM(a.time_spent),2)
+AS open_perc
+FROM activities AS a
+INNER JOIN age_breakdown AS b
+ON a.user_id=b.user_id
+GROUP BY b.age_bucket; --cau nay em submit thi bi sai nhung em ko biet sai o dau a :<
+--EX4
