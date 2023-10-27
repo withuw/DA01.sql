d52300cf434f9787314d78effddd61111753adf6
withuw <147489559+withuw@users.noreply.github.com>
2023-10-27 18:18:57 +0300

    Update MIDCOURSE.sql

--- MIDCOURSE.sql
+++ MIDCOURSE.sql
@@ -12,7 +12,16 @@ sum(case when replacement_cost
 from film
 
 --Q3
-
+select c.title,c.length,a.name 
+from category as a
+inner join film_category as b
+on a.category_id=b.category_id
+inner join film as c
+on b.film_id=c.film_id
+where a.name='Sports' or a.name='Drama'
+order by length desc
+limit 1
+	
 --Q4
 select name,
 count(name)
@@ -42,3 +51,32 @@ on a.address_id=b.address_id
 where b.address_id is null
 
 --Q7
+select city.city, sum(payment.amount) as sum
+from payment
+inner join customer
+on payment.customer_id=customer.customer_id
+inner join address
+on
+customer.address_id=address.address_id
+inner join city
+on address.city_id=city.city_id
+group by city.city
+order by sum desc
+limit 1
+
+--Q8
+select concat(city.city,', ',country.country),
+sum(payment.amount) as sum
+from payment
+inner join customer
+on payment.customer_id=customer.customer_id
+inner join address
+on
+customer.address_id=address.address_id
+inner join city
+on address.city_id=city.city_id
+inner join country
+on city.country_id=country.country_id
+group by city.city, country.country
+order by sum asc
+limit 1
