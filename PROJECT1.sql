2c2a51d09a4ad036edb9da168bbd143e3606c062
withuw <147489559+withuw@users.noreply.github.com>
2024-04-11 23:05:12 +0300

    Update PROJECT1.sql

--- PROJECT1.sql
+++ PROJECT1.sql
@@ -1,5 +1,5 @@
 SELECT * FROM public.sales_dataset_rfm_prj;
---Chuyển đổi kiểu dữ liệu phù hợp cho các trường ( sử dụng câu lệnh ALTER) 
+---Change appropriate data type for columns
 ALTER TABLE public.sales_dataset_rfm_prj
 ALTER COLUMN orderdate TYPE date USING (trim(orderdate)::date)
 ALTER COLUMN quantityordered TYPE numeric USING (trim(quantityordered)::numeric),
@@ -11,7 +11,7 @@ ALTER COLUMN city TYPE text USING (trim(city)::text),
 ALTER COLUMN state TYPE text USING (trim(state)::text),
 ALTER COLUMN country TYPE text USING (trim(country)::text)
 
---Check NULL/BLANK (‘’)  ở các trường: ORDERNUMBER, QUANTITYORDERED, PRICEEACH, ORDERLINENUMBER, SALES, ORDERDATE.
+--Check for NULL/BLANK (‘’)  in: ORDERNUMBER, QUANTITYORDERED, PRICEEACH, ORDERLINENUMBER, SALES, ORDERDATE.
 SELECT * from sales_dataset_rfm_prj
 WHERE ordernumber IS NULL OR
 quantityordered IS NULL OR
@@ -20,7 +20,7 @@ orderlinenumber IS NULL OR
 sales IS NULL OR
 orderdate IS NULL
 
---Thêm cột CONTACTLASTNAME, CONTACTFIRSTNAME được tách ra từ CONTACTFULLNAME 
+---Add column CONTACTLASTNAME, CONTACTFIRSTNAME from CONTACTFULLNAME 
 ALTER TABLE public.sales_dataset_rfm_prj
 ADD column contactfirstname VARCHAR (50)
 
@@ -33,7 +33,7 @@ ADD column contactlastname VARCHAR (50)
 UPDATE sales_dataset_rfm_prj
 SET contactlastname = RIGHT(contactfullname, LENGTH(contactfullname) - POSITION ('-' IN contactfullname))
 
---Chuẩn hóa CONTACTLASTNAME, CONTACTFIRSTNAME theo định dạng chữ cái đầu tiên viết hoa, chữ cái tiếp theo viết thường. 
+---Update CONTACTLASTNAME, CONTACTFIRSTNAME to capitalize the first letter and make the subsequent letters lowercase. 
 
 UPDATE sales_dataset_rfm_prj
 SET contactlastname= INITCAP(contactlastname)
@@ -41,7 +41,7 @@ SET contactlastname= INITCAP(contactlastname)
 UPDATE sales_dataset_rfm_prj
 SET contactfirstname= INITCAP(contactfirstname)
 
---Thêm cột QTR_ID, MONTH_ID, YEAR_ID lần lượt là Qúy, tháng, năm được lấy ra từ ORDERDATE 
+---Add columns QTR_ID, MONTH_ID, YEAR_ID, which are quarters, months, years extracted from ORDERDATE 
 
 ALTER TABLE sales_dataset_rfm_prj
 ADD COLUMN QTR_ID numeric, 
@@ -53,9 +53,9 @@ SET QTR_ID= EXTRACT (quarter FROM orderdate),
 MONTH_ID= EXTRACT (month FROM orderdate),
 YEAR_ID= EXTRACT (year FROM orderdate)
 
---Hãy tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách)
+---Find outliers for column QUANTITYORDERED (2 ways)
 
---C1:Boxplot
+----1: Using Boxplot
 
 WITH min_max_values AS(
 SELECT Q1 - 1.5*IQR AS min_value,
@@ -72,7 +72,7 @@ SELECT * FROM sales_dataset_rfm_prj
 WHERE quantityordered < (select min_value from min_max_values)
 OR quantityordered > (select max_value from min_max_values)
 
---C2: Z-Score
+---2: Using Z-Score
 
 with base as (
 select quantityordered,
@@ -83,36 +83,4 @@ from sales_dataset_rfm_prj)
 select *, (quantityordered-avg)/stddev as z_score
 from base
 where abs((quantityordered-avg)/stddev)>3
-
---DELETE
-
- DELETE FROM sales_dataset_rfm_prj
- WHERE quantityordered IN (WITH min_max_values AS(
-SELECT Q1 - 1.5*IQR AS min_value,
-Q3 + 1.5*IQR AS max_value
-FROM
-(SELECT 
-PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY quantityordered) AS Q1,
-PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY quantityordered) AS Q3,
-PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY quantityordered)
-- PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY quantityordered) AS IQR
-FROM sales_dataset_rfm_prj) as a)
-
-SELECT * FROM sales_dataset_rfm_prj
-WHERE quantityordered < (select min_value from min_max_values)
-OR quantityordered > (select max_value from min_max_values))
-
---
-DELETE FROM sales_dataset_rfm_prj
-WHERE quantityordered IN (
-with base as (
-select quantityordered,
-(select avg(quantityordered) from sales_dataset_rfm_prj as avg),
-(select stddev(quantityordered) from sales_dataset_rfm_prj as stddev)
-from sales_dataset_rfm_prj)
-
-select *, (quantityordered-avg)/stddev as z_score
-from base
-where abs((quantityordered-avg)/stddev)>3
-)
 
