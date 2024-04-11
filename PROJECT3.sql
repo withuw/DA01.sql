227b36c47aa03af9d9abbb33c7245fcdf44bf28e
withuw <147489559+withuw@users.noreply.github.com>
2024-04-11 23:34:53 +0300

    Update PROJECT3.sql

--- PROJECT3.sql
+++ PROJECT3.sql
@@ -10,7 +10,7 @@ select *, (quantityordered-avg)/stddev as z_score
 from base
 where abs((quantityordered-avg)/stddev)<=3)
   
-----Doanh thu theo từng ProductLine, Year  và DealSize?
+----Revenue grouped by ProductLine, Year and DealSize?
 
 select 
 productline, year_id, dealsize,
@@ -18,7 +18,7 @@ sum(sales) as revenue
 from clean_database
 group by productline, year_id, dealsize
 
----Đâu là tháng có bán tốt nhất mỗi năm?
+---Which is the month that has most sales?
 select
 month_id,
 sum(sales) as revenue,
@@ -28,7 +28,7 @@ group by month_id
 order by revenue desc, order_number desc
 limit 1
 
----Product line nào được bán nhiều ở tháng 11?
+---Which Product line was sold the most on November?
 select productline, month_ID, 
 sum(sales) as revenue,
 count(ordernumber) as ORDER_NUMBER 
@@ -38,7 +38,7 @@ group by productline, month_ID
 order by sum(sales) desc , count(ordernumber) desc 
 limit 1
 
----Đâu là sản phẩm có doanh thu tốt nhất ở UK mỗi năm? Xếp hạng các các doanh thu đó theo từng năm.
+---Which product has the most revenue in UK per year? Rank the revenue per year.
 ranking as(
 select year_id, productline,
 sum(sales) as revenue,
@@ -50,22 +50,22 @@ group by year_id, productline)
 select * from ranking
 where rank=1
 
----Ai là khách hàng tốt nhất, phân tích dựa vào RFM 
+---RFM Analysis
 with RFM_calc as 
 (select 
 contactfullname,
-current_date - max(orderdate) as R,
-count(distinct ordernumber) as F,
-sum(sales) as M 
+current_date - max(orderdate) as Recency,
+count(distinct ordernumber) as Frequency,
+sum(sales) as Monetary
 from clean_database
 group by contactfullname),
 
 scores as 
 (select contactfullname,
-ntile(5) over(order by R desc) as
+ntile(5) over(order by Recency desc) as
 R_score,
-ntile(5) over(order by F ) as F_score,
-ntile(5) over(order by M ) as M_score
+ntile(5) over(order by Frequency ) as F_score,
+ntile(5) over(order by Monetary ) as M_score
 from cte),
   
 segmentation as
